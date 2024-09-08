c70a2d19af3a345e3baafd284c632248704357f8
grymmjack <grymmjack@gmail.com>
2024-09-08 15:44:53 -0400

    Added basic enemy AI and shooting

--- enemy.gd
+++ enemy.gd
@@ -4,6 +4,8 @@ extends Area2D
 @export var speed:int = 150
 @export var rotation_speed:int = 120
 @export var health:int = 3
+@export var bullet_spread:float = 0.2
+
 
 var follow = PathFollow2D.new()
 var target = null
@@ -24,4 +26,46 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_gun_cooldown_timeout() -> void:
-	pass # Replace with function body.
+	var shot_type:int = randi_range(1, 6)
+	if shot_type > 5:
+		shoot_pulse(3, 0.15)
+	else:
+		shoot()
+
+
+func shoot() -> void:
+	var dir = global_position.direction_to(target.global_position)
+	dir = dir.rotated(randf_range(-bullet_spread, bullet_spread))
+	var b = bullet_scene.instantiate()
+	get_tree().root.add_child(b)
+	b.start(global_position, dir)
+
+
+func shoot_pulse(n, delay) -> void:
+	for i in n:
+		shoot()
+		await get_tree().create_timer(delay).timeout
+
+
+func take_damage(amount:int) -> void:
+	health -= amount
+	$AnimationPlayer.play("flash")
+	if health <= 0:
+		explode()
+
+
+func explode() -> void:
+	speed = 0
+	$GunCooldown.stop()
+	$CollisionShape2D.set_deferred("disabled", "true")
+	$Sprite2D.hide()
+	$Explosion.show()
+	$Explosion/AnimationPlayer.play("explosion")
+	await $Explosion/AnimationPlayer.animation_finished
+	queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("rocks"):
+		return
+	explode()
---
+++ enemy_bullet.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+@export var speed:int = 1000
+
+
+func start(_pos, _dir):
+	position = _pos
+	rotation = _dir.angle()
+
+
+func _process(delta: float) -> void:
+	position += transform.x * speed * delta
+
+
+func _on_body_entered(body):
+	queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
